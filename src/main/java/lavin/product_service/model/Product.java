4ba842cdbe19141597d3d798bae28ac7d8cc8f7c
Larissa Fernandes <l.fernandes0308@gmail.com>
2024-08-01 17:08:08 -0300

    ConCommit da Class Product

---
+++ src/main/java/lavin/product_service/model/Product.java
@@ -0,0 +1,20 @@
+package lavin.product_service.model;
+
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+
+@Data
+@Document()
+public class Product {
+    @Id
+    private String id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Integer stock;
+
+}
