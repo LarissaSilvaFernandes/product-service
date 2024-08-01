89de940045cc9a3fc259e8279c54ec38252f12d7
Larissa Fernandes <l.fernandes0308@gmail.com>
2024-08-01 17:18:50 -0300

    commit de ProductController

---
+++ src/main/java/lavin/product_service/controller/ProductController.java
@@ -0,0 +1,10 @@
+package lavin.product_service.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+}
